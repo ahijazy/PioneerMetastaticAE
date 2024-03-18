23f4fa5916f2af577797a74e9f0386198254fe27
ahijazy <94984907+ahijazy@users.noreply.github.com>
2024-03-18 09:36:24 +0100

    Delete inst directory

--- inst/settings/crossing targets and outcomes.R
+++
@@ -1,7 +0,0 @@
-target=read.csv("./inst/settings/targets.csv")  
-outcome=read.csv("./inst/settings/outcomes.csv")
-crossed <- merge(target, outcome, by = NULL)
-
-names(crossed)=c("target_id","target_name","outcome_id","outcome_name","episodic","chronic")
-crossed <- crossed[order(crossed$target_id), ]
-write.csv(x=crossed,file = "inst/settings/IRsettings.csv")
